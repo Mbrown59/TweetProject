d00f512da19d434274a3e30815ae277344e5646d
Mbrown59 <mbrown59@cub.uca.edu>
2021-09-23 18:04:43 -0500

    The file reader is kinda working

--- Project1/TweetsPackage/Main.java
+++ Project1/TweetsPackage/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		System.out.println("hello");
+		//Tweet t1 = new Tweet();
+		Twitter allTweets = new Twitter("allTweets" , " ./TweetsPackage/Tweets.txt");
+		
+		System.out.println(allTweets);
 	}
 
 }
--- Project1/TweetsPackage/Twitter.java
+++ Project1/TweetsPackage/Twitter.java
@@ -1,23 +1,70 @@
 package TweetsPackage;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Twitter
 {
 	
 	private String fileName;
+	private String className;
 	private Tweet newTweet;
 	
 	public Twitter()
 	{
 		fileName = null;
+		className = "a class";
 	}
 	
-	public Twitter(String fn)
+	public Twitter(String fn, String cn)
 	{
 		fileName = fn;
+		className = cn;
+		readFile();
+		
 	}
+	
+	private void readFile () {
+		BufferedReader lineReader = null;
+		try {
+			FileReader fr = new FileReader(fileName);
+			lineReader = new BufferedReader(fr);
+			String line = null;
+			while ((line = lineReader.readLine())!=null) {
+				String tweet = lineReader.readLine();
+					}
+		} catch (Exception e) {
+			System.err.println("there was a problem with the file reader, try different read type.");
+			try {
+				lineReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(fileName.substring(1))));
+				String line = null;
+				while ((line = lineReader.readLine())!=null) {
+					String tweet = lineReader.readLine();
+					String id = lineReader.readLine();
+					}
+			} catch (Exception e2) {
+				System.err.println("there was a problem with the file reader, try again.  either no such file or format error");
+			} finally {
+				if (lineReader != null)
+					try {
+						lineReader.close();
+					} catch (IOException e2) {
+						System.err.println("could not close BufferedReader");
+					}
+			}			
+		} finally {
+			if (lineReader != null)
+				try {
+					lineReader.close();
+				} catch (IOException e) {
+					System.err.println("could not close BufferedReader");
+				}
+		}
+	} // end of readFile method	
 	
 	private void writeFile(String fn)
 	{
